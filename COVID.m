b7ae765b90040d43b311b320d40639959f487cde
Puranam Revanth Kumar <75935999+PuranamRevanthKumar@users.noreply.github.com>
2022-06-08 09:38:07 +0530

    Add files via upload
    https://www.kaggle.com/datasets/tawsifurrahman/covid19-radiography-database

---
+++ COVID.m
@@ -0,0 +1,150 @@
+% Clear workspace
+clear; close all; clc;
+ 
+% Images Datapath – Please modify your path accordingly 
+datapath='D:\Conference and Journals\Revanth Conferences Completed\5th Paper\Dataset_A';
+ 
+% Image Datastore
+imds=imageDatastore(datapath, ...
+    'IncludeSubfolders',true, ...
+    'LabelSource','foldernames');
+% Determine the split up
+total_split=countEachLabel(imds)
+
+% Number of Images
+num_images=length(imds.Labels);
+
+% Visualize random images
+perm=randperm(num_images,4);
+figure;
+for idx=1:length(perm)
+    
+    subplot(2,2,idx);
+    imshow(imread(imds.Files{perm(idx)}));
+    title(sprintf('%s',imds.Labels(perm(idx))))
+    
+end
+%%
+% Number of folds
+num_folds=2;
+
+% Loop for each fold
+for fold_idx=1:num_folds;
+    
+    fprintf('Processing %d among %d folds \n',fold_idx,num_folds);
+    
+   % Test Indices for current fold
+    test_idx=fold_idx:num_folds:num_images;
+
+    % Test cases for current fold
+    imdsTest = subset(imds,test_idx);
+    
+    % Train indices for current fold
+    train_idx=setdiff(1:length(imds.Files),test_idx);
+    
+    % Train cases for current fold
+    imdsTrain = subset(imds,train_idx);
+ 
+    % ResNet Architecture 
+    net=resnet50;
+    lgraph = layerGraph(net);
+    clear net;
+    
+    % Number of categories
+    numClasses = numel(categories(imdsTrain.Labels));
+    
+    % New Learnable Layer
+    newLearnableLayer = fullyConnectedLayer(numClasses, ...
+        'Name','new_fc', ...
+        'WeightLearnRateFactor',10, ...
+        'BiasLearnRateFactor',10);
+    
+    % Replacing the last layers with new layers
+    lgraph = replaceLayer(lgraph,'fc1000',newLearnableLayer);
+    newsoftmaxLayer = softmaxLayer('Name','new_softmax');
+    lgraph = replaceLayer(lgraph,'fc1000_softmax',newsoftmaxLayer);
+    newClassLayer = classificationLayer('Name','new_classoutput');
+    lgraph = replaceLayer(lgraph,'ClassificationLayer_fc1000',newClassLayer);
+ 
+%     % Preprocessing Technique
+     imdsTrain.ReadFcn = @(filename)preprocess_Xray(filename);
+     imdsTest.ReadFcn = @(filename)preprocess_Xray(filename);
+    
+    % Training Options, we choose a small mini-batch size due to limited images 
+    options = trainingOptions('adam',...
+        'MaxEpochs',5,'MiniBatchSize',25,...
+        'Shuffle','every-epoch', ...
+        'InitialLearnRate',0.001, ...
+        'Verbose',false, ...
+        'Plots','training-progress');
+%     options=trainingOptions('adam',...
+%         'InitialLearnRate',0.001,'MaxEpochs',3,'MiniBatchSize',25,...
+%         'Shuffle','every-epoch','validationFrequency',20,...
+%      'Verbose',false,'Plots','training-progress');
+    
+    
+    % Data Augumentation
+    augmenter = imageDataAugmenter( ...
+        'RandRotation',[-5 5],'RandXReflection',1,...
+        'RandYReflection',1,'RandXShear',[-0.05 0.05],'RandYShear',[-0.05 0.05]);
+    
+    % Resizing all training images to [224 224] for ResNet architecture
+    auimds = augmentedImageDatastore([224 224],imdsTrain,'DataAugmentation',augmenter);
+    
+    % Training
+    netTransfer = trainNetwork(auimds,lgraph,options);
+    
+    % Resizing all testing images to [224 224] for ResNet architecture   
+    augtestimds = augmentedImageDatastore([224 224],imdsTest);
+   
+    % Testing and their corresponding Labels and Posterior for each Case
+    [predicted_labels(test_idx),posterior(test_idx,:)] = classify(netTransfer,augtestimds);
+    
+    % Save the Independent ResNet Architectures obtained for each Fold
+    save(sprintf('ResNet50_%d_among_%d_folds',fold_idx,num_folds),'netTransfer','test_idx','train_idx');
+    
+    % Clearing unnecessary variables 
+    clearvars -except fold_idx num_folds num_images predicted_labels posterior imds netTransfer;
+   
+% Actual Labels
+actual_labels=imds.Labels;
+
+% Confusion Matrix
+figure;
+cm = confusionchart(actual_labels,predicted_labels','DiagonalColor',[0 0.4470 0.7410])
+%plotconfusion(actual_labels,predicted_labels','blue')
+title('Confusion Matrix for XceptionResNet50V2 - Fold 2');
+    
+end
+
+%%
+test_labels=double(nominal(imds.Labels));
+
+% ROC Curve - Our target class is the first class in this scenario 
+[fp_rate,tp_rate,T,AUC]=perfcurve(test_labels,posterior(:,1),1);
+figure;
+plot(fp_rate,tp_rate,'g-');
+grid on;
+xlabel('False Positive Rate');
+ylabel('Detection Rate');
+% Area under the ROC curve value
+AUC
+%%
+function Iout = preprocess_Xray(filename)
+% This function preprocesses the given X-ray image by converting it into
+% grayscale if required and later converting to 3-channel image to adapt to
+% existing deep learning architectures 
+%
+
+% Read the Filename
+I = imread(filename);
+
+% Some images might be RGB, convert them to Grayscale
+if ~ismatrix(I)
+    I=rgb2gray(I); 
+end
+
+% Replicate the image 3 times to create an RGB image
+Iout = cat(3,I,I,I);
+
+end
